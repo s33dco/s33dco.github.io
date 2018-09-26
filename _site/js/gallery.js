478862061b91d8d46ae7855eb0fbf97f7b8dddbd
s33dco <code@s33d.co>
2018-09-26 12:47:12 +0100

    Finish the JavaScript gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,23 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail){
+  	thumbnail.addEventListener("click", function(){
+  		// Set clicked image as main image.
+  		let newImageSrc = thumbnail.dataset.largeVersion;
+  		mainImage.setAttribute("src", newImageSrc);
+  		mainImage.setAttribute("alt", thumbnail.alt);
+  		// change which image is current
+  		let currentClass = "current";
+  		document.querySelector(".current").classList.remove(currentClass);
+  		thumbnail.parentNode.classList.add(currentClass);
+  		// change gallery-info
+  		let galleryInfo = document.querySelector("#gallery-info");
+  		let title       = galleryInfo.querySelector(".title");
+  		let description = galleryInfo.querySelector(".description");
+  		title.innerHTML       = thumbnail.dataset.title;
+  		description.innerHTML = thumbnail.dataset.description;
+  	});
+  });
 }
