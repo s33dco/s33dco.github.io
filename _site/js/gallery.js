be2b2d806af22ec83bbc1ef813de43141e30ac9e
s33dco <code@s33d.co>
2018-09-26 12:59:09 +0100

    preload images

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -3,15 +3,23 @@ function activateGallery() {
   let mainImage  = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail){
+
+  	// Preload large images.
+  	let newImageSrc  = thumbnail.dataset.largeVersion;
+  	let largeVersion = new Image();
+  	largeVersion.src = newImageSrc;
+
   	thumbnail.addEventListener("click", function(){
+
   		// Set clicked image as main image.
-  		let newImageSrc = thumbnail.dataset.largeVersion;
   		mainImage.setAttribute("src", newImageSrc);
   		mainImage.setAttribute("alt", thumbnail.alt);
+
   		// change which image is current
   		let currentClass = "current";
   		document.querySelector(".current").classList.remove(currentClass);
   		thumbnail.parentNode.classList.add(currentClass);
+
   		// change gallery-info
   		let galleryInfo = document.querySelector("#gallery-info");
   		let title       = galleryInfo.querySelector(".title");
